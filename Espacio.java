7facb96f6ed29c2174287835c40e7dea30b7829c
anzagood1 <anzagood@espol.edu.ec>
2024-11-24 17:15:44 -0500

    Update Espacio.java

--- Espacio.java
+++ Espacio.java
@@ -6,7 +6,15 @@ public class Espacio {
     private String nombre;
     private int capacidad;
     private EstadoEspacio estado;
-
+    
+    public Espacio(String codigoUnico,TipoEspacio tipoDeEspacio, String nombre, int capacidad,  EstadoEspacio estado ){
+        this.codigoUnico = codigoUnico;
+        this.tipoDeEspacio = tipoDeEspacio;
+        this.nombre = nombre;
+        this.capacidad = capacidad;
+        this.estado = estado;
+    }
+    
     public void setCodigoUnico(String codigoUnico){
         this.codigoUnico = codigoUnico;
     }

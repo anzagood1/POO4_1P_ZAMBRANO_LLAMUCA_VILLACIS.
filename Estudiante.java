d371968dd002ceb175c7fe9b84fa7dc3ae1461ef
LisbethLlamuca <lisbllam@espol.edu.ec>
2024-11-24 16:49:50 -0500

    Update Estudiante.java
    consultarReserva estudiante

--- Estudiante.java
+++ Estudiante.java
@@ -13,7 +13,24 @@ public class Estudiante extends Usuario{
   }
 
   public void consultarReserva(){
-    super.consultarReserva();
+    for (Reserva reserva: reservas){
+      if (reserva.getFechaReserva()==fecha && reserva.getUsuario().getCodigoUnico()==this.getCodigoUnico()){
+        Espacio espacio;
+        for (Espacio espa: espacios){
+          if (espa.getCodigoUnico()==reserva.getCodigoUnicoEspacio()){
+            espacio=espa;
+          }
+        }
+
+        System.out.println("Código de Reserva: "+reserva.getCodigoReserva());
+        System.out.println("Fecha de Reserva: "+reserva.getFechaReserva());
+        System.out.println("Tipo de Espacio: "+reserva.getTipoDeEspacio());
+        System.out.println("Nombre de Espacio"+espacio.getNombre());
+        System.out.println("Capacidad de Espacio: "+espacio.getCapacidad());
+        System.out.println("Nombres y Apellidos: "+reserva.getUsuario().getNombres()+reserva.getUsuario().getApellidos());
+        System.out.println("Estado de reserva: "+reserva.getEstadoDeLaReserva());
+      }
+    }
   }
 
   public void setnumMatricula(int numMatricula){

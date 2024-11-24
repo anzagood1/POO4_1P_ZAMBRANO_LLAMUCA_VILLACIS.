6808629fca956c3168da51b4ebd5dbf148ef7a2b
LisbethLlamuca <lisbllam@espol.edu.ec>
2024-11-24 16:45:09 -0500

    Update Administrador.java
    consultarReserva de administrador

--- Administrador.java
+++ Administrador.java
@@ -8,7 +8,23 @@ public class Administrador extends Usuario{
         this.cargoAdmin = cargoAdmin;
     }
 
-    public void consultarReserva(){}
+    public void consultarReserva(){
+        System.out.println("Números de reservas: "+Reserva.numeroReservas);
+        System.out.println("Reservas de Estudiantes: ");
+        System.out.println("CÓDIGO RESERVA - ESTADO - FECHA - NOMBRES Y APELLIDOS - MATRÍCULA");
+        for (Reserva reserva: reservas){
+            if(reserva.getUsuario() instanceof Estudiante){
+                System.out.println(reserva.getCodigoReserva()+" - "+reserva.getEstadoDeLaReserva()+" - "+reserva.getFechaReserva+" - "+reserva.getUsuario().getNombres()+" "+reserva.getUsuario().getApellidos()+" - "+reserva.getUsuario().getnumMatricula());
+            }
+        }
+        System.out.println("Reservas de Profesores: ");
+        System.out.println("CÓDIGO RESERVA - ESTADO - FECHA - NOMBRES Y APELLIDOS - MATERIA");
+        for (Reserva reserva: reservas){
+            if(reserva.getUsuario() instanceof Profesor){
+                System.out.println(reserva.getCodigoReserva()+" - "+reserva.getEstadoDeLaReserva()+" - "+reserva.getFechaReserva+" - "+reserva.getUsuario().getNombres()+" "+reserva.getUsuario().getApellidos()+" - "+reserva.getCodigoUnicoEspacio());
+            }
+        }
+    }
 
     public void gestionarReserva(){}
 }

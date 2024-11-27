c29e35bf04a1bbf1e71b87b5da25beab969caf46
LisbethLlamuca <lisbllam@espol.edu.ec>
2024-11-26 21:37:02 -0500

    Update Estudiante.java

--- src/Personas/Estudiante.java
+++ src/Personas/Estudiante.java
@@ -6,16 +6,37 @@ public class Estudiante extends Usuario{
   private int numMatricula;
   private String carrera;
 
+  /**
+   * Este método es el constructor de la subclase Estudiantes.
+   * @param codigoUnico codigo unico del usuario.
+   * @param cedula cedula del usuario.
+   * @param nombres nombres del usuario.
+   * @param apellidos apellidos del usuario.
+   * @param usuario nombre del usuario.
+   * @param contrasenia contrasenia del usuario.
+   * @param correo correo del usuario.
+   * @param numMatricula número de matricula del estudiante. 
+   * @param carrera carrera que estudia el estudiante.
+   * @return no retorna valor, es un constructor.
+   */
   public Estudiante(String codigoUnico, String cedula, String nombres, String apellidos, String usuario, String contrasenia, String correo, int numMatricula, String carrera){
     super(codigoUnico, cedula,nombres, apellidos, usuario, contrasenia, correo)
     this.numMatricula= numMatricula;
     this.carrera= carrera;
   }
 
+  /*
+   * Este método permite al Estudiante realizar una reserva de un espacio.
+  */
   public void gestionarReserva(){
     super.gestionarReserva();
   }
 
+  /*
+   * Este método permite al estudiante consultar una reserva que haya realizado previamente.
+   * @param fecha es la fecha de la reserva a consultar.
+   * @return no retorna valores, imprime en consola.
+   */
   public void consultarReserva(){
     for (Reserva reserva: reservas){
       if (reserva.getFechaReserva()==fecha && reserva.getUsuario().getCodigoUnico()==this.getCodigoUnico()){
@@ -37,6 +58,7 @@ public class Estudiante extends Usuario{
     }
   }
 
+  //setters
   public void setnumMatricula(int numMatricula){
     this.numMatricula= numMatricula;
   }
@@ -45,6 +67,7 @@ public class Estudiante extends Usuario{
     this.carrera= carrera;
   }
 
+  //getters
   public int getnumMatricula(){
     return numMatricula;
   }

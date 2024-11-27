80c25ef70793f67366c62e763ecec71d2db23a04
SantiagoVillacis <savihuer@espol.edu.ec>
2024-11-27 07:08:46 -0500

    Update Usuario.java

--- src/Personas/Usuario.java
+++ src/Personas/Usuario.java
@@ -9,8 +9,8 @@ import javax.mail.internet.MimeMessage;
 import io.github.cdimascio.dotenv.Dotenv;
 import javax.mail.*;
 import java.util.Scanner;
-import src.Funcionalidades.*
-import src.ENUMS.*
+import src.Funcionalidades.*;
+import src.ENUMS.*;
 
 public abstract class Usuario{
   protected String codigoUnico;

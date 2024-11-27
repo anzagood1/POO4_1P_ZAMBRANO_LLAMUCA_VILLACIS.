b72b5f1cf0099c790aaf1fc06c81032288313e55
LisbethLlamuca <lisbllam@espol.edu.ec>
2024-11-26 21:42:39 -0500

    Update Administrador.java

--- src/Personas/Administrador.java
+++ src/Personas/Administrador.java
@@ -6,11 +6,26 @@ import ENUMS.Cargo;
 public class Administrador extends Usuario{
     Cargo cargoAdmin;
 
+    /**
+     * Este método es el constructor de la subclase Administrador.
+     * @param codigoUnico codigo unico del usuario.
+     * @param cedula cedula del usuario.
+     * @param nombres nombres del usuario.
+     * @param apellidos apellidos del usuario.
+     * @param usuario nombre del usuario.
+     * @param contrasenia contrasenia del usuario.
+     * @param correo correo del usuario.
+     * @param cargoAdmin cargo del administrador
+     * @return no retorna valores, es un constructor.
+     */
     public Administrador(String codigoUnico, String cedula, String nombres, String apellidos, String usuario, String contrasenia, String correo, Cargo cargoAdmin){
         super(codigoUnico, cedula,nombres, apellidos, usuario, contrasenia, correo);
         this.cargoAdmin = cargoAdmin;
     }
 
+    /**
+     * Este método permite al administrador visualizar todas las reservas con los datos de estas.
+     */
     public void consultarReserva(){
         System.out.println("Números de reservas: "+Reserva.numeroReservas);
         System.out.println("Reservas de Estudiantes: ");
@@ -29,5 +44,8 @@ public class Administrador extends Usuario{
         }
     }
 
+    /**
+     * Método que permite al admnistrador aprobar o rechazar las reservas.
+     */
     public void gestionarReserva(){}
 }

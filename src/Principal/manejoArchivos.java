1c0242b96ec85daccead469dcb28971d769c21f9
anzagood1 <anzagood@espol.edu.ec>
2024-11-27 19:31:09 -0500

    Update manejoArchivos.java

--- src/Principal/manejoArchivos.java
+++ src/Principal/manejoArchivos.java
@@ -58,7 +58,6 @@ public class manejoArchivos {
             fichero = new FileWriter(nombreArchivo,true);
             bw = new BufferedWriter(fichero);
             bw.write(linea+"\n");
-            System.out.println("ksdsdlsd");
 
         } catch (Exception e) {
             e.printStackTrace();
